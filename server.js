04f83858ad7a73af042c8b299d2fea88044c3582
TuanLeAnh1003 <19520331@gm.uit.edu.vn>
2021-12-05 23:42:08 +0700

    abc

--- server.js
+++ server.js
@@ -37,9 +37,45 @@ app.use(passport.session);
 
 require('./routes/user')(app);
 
-app.listen('3000', function () {
-  console.log('Listening on port 3000!');
+// app.listen('3000', function () {
+//   console.log('Listening on port 3000!');
+// });
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`server stared on port ${PORT}`);
 });
 
+app.get('/phim', async(req, res) => {
+  var pool = await conn;
+  var sqlString = "select TENPHIM, DOTUOI, MOTA, THELOAI " + 
+                  "from PHIM P join PHIM_THELOAI PTL on P.MAPHIM = PTL.MAPHIM " + 
+                  "join THELOAI TL on TL.MATL = PTL.MATL"
+  return await pool.request()
+  .query(sqlString)
+  .then(data => {
+    if(data.recordset.length >= 1) {
+      res.send(data.recordset);
+    } else res.send('0');
+  })
+  .catch(err => res.send('0'));
+})
 
-
+app.get('/phim/:phim', async(req, res) => {
+  var pool = await conn;
+  var name = req.params.phim.slice(1);
+  var sqlString = `select * from PHIM join PHIM_THELOAI 
+                  on PHIM.MAPHIM = PHIM_THELOAI.MAPHIM 
+                  join THELOAI on THELOAI.MATL = PHIM_THELOAI.MATL 
+                  where TENPHIM like N'%${name}%'`;
+  return await pool.request()
+  .query(sqlString)
+  .then(data => {
+    res.send(data);
+    console.log(data);
+  })
+  .catch(err => {
+    res.send(err);
+    console.log(err);
+  })
+});
