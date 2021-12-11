4ea743492ba75707fef2f6393eba9ac85d7ffd8b
TuanLeAnh1003 <19520331@gm.uit.edu.vn>
2021-12-11 16:53:26 +0700

    make it better

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const PORT = 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server stared on port ${PORT}`);
 });
@@ -28,3 +28,22 @@ app.get('/phim', async(req, res) => {
   })
   .catch(err => res.send('0'));
 })
+
+app.get('/phim/:phim', async(req, res) => {
+  var pool = await conn;
+  var name = req.params.phim.slice(1);
+  var sqlString = `select * from PHIM join PHIM_THELOAI 
+                  on PHIM.MAPHIM = PHIM_THELOAI.MAPHIM 
+                  join THELOAI on THELOAI.MATL = PHIM_THELOAI.MATL 
+                  where TENPHIM like N'%${name}%'`;
+  return await pool.request()
+  .query(sqlString)
+  .then(data => {
+    res.send(data);
+    console.log(data);
+  })
+  .catch(err => {
+    res.send(err);
+    console.log(err);
+  })
+});
